1aa2ed98b8c39d727cf985495950d9c3efa14372
Young-Seok-Kim <x1682@naver.com>
2026-05-21 00:12:33 +0900

    로그아웃시바텀시트가 닫히도록 수정

--- app/src/main/java/com/youngs/dailynet/ui/MainActivity.kt
+++ app/src/main/java/com/youngs/dailynet/ui/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.youngs.dailynet.ui.theme.DailyNetTheme
 import com.youngs.dailynet.ui.view.LoginScreen
@@ -32,6 +33,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
         setContent {
             DailyNetTheme {
