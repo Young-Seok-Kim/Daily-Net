2262d6d2c3d00071f2c1c973586704f6fd1dfd8f
Young-Seok-Kim <x1682@naver.com>
2026-05-21 17:05:46 +0900

    user 정보 이름도 저장되도록 수정

--- app/src/main/java/com/youngs/dailynet/data/local/entity/UserProfileEntity.kt
+++ app/src/main/java/com/youngs/dailynet/data/local/entity/UserProfileEntity.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "user_profile")
 data class UserProfileEntity(
     @PrimaryKey val id: Int = 0, // 단일 행만 유지
+    val name: String = "",
     val height: Float = 0f,
     val isMale: Boolean = true,
     val initialWeight: Float,
