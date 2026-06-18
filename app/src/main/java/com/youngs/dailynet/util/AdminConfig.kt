d7959f83b7cf17f9f76c87c9b1e3d1de4226c12c
Young-Seok-Kim <x1682@naver.com>
2026-06-18 23:00:47 +0900

    어드민 이메일 계정 추가

--- app/src/main/java/com/youngs/dailynet/util/AdminConfig.kt
+++ app/src/main/java/com/youngs/dailynet/util/AdminConfig.kt
@@ -2,7 +2,7 @@ package com.youngs.dailynet.util
 
 object AdminConfig {
     // 본인의 이메일을 여기에 넣으세요.
-    private val adminEmails = listOf("seok9562@gmail.com")
+    private val adminEmails = listOf("seok9562@gmail.com", "sjyy9797@gmail.com")
 
     fun isUserAdmin(email: String?): Boolean {
         return email != null && adminEmails.contains(email)
