a92af7410256fafa5fea2282257948f4d4986cd0
Young-Seok-Kim <x1682@naver.com>
2026-06-18 15:05:49 +0900

    구독여부  firebase에 저장되도록 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.youngs.dailynet"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
